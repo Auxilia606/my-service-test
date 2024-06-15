a03963fdf5b423fcdfc698a2167761d87edf70ba
DESKTOP-61B6P5C\auxil <auxilia606@gmail.com>
2024-06-15 14:37:48 +0900

    Feat : 로그인 페이지, 회원가입 페이지 디자인 구현 완료

--- frontend/src/pages/SignUp/page.tsx
+++ frontend/src/pages/SignUp/page.tsx
@@ -1,3 +1,60 @@
+import { useNavigate } from "react-router-dom";
+import Button from "antd/es/button";
+import Form from "antd/es/form";
+import Input from "antd/es/input";
+
+import { Page } from "@shared/components/Page";
+
 export const SignUp: React.FC = () => {
-  return <div>SignUp Page</div>;
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  console.log(navigate);
+
+  return (
+    <Page>
+      <Page.Header back title="회원가입" />
+      <Page.Body>
+        <Form form={form}>
+          <Form.Item
+            label="아이디"
+            name="id"
+            rules={[{ required: true, message: "아이디를 입력해주세요" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="비밀번호"
+            name="password"
+            rules={[{ required: true, message: "비밀번호를 입력해주세요" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="비밀번호 확인"
+            name="password-confirm"
+            rules={[
+              { required: true, message: "비밀번호를 입력해주세요" },
+              ({ getFieldValue }) => ({
+                validator: (_, value) => {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error("비밀번호가 일치하지 않습니다")
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item noStyle>
+            <Button type="primary" htmlType="submit" className="my-4">
+              가입하기
+            </Button>
+          </Form.Item>
+        </Form>
+      </Page.Body>
+    </Page>
+  );
 };
