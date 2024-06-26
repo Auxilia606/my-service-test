062894363f70ad5efbcc84ff34cc8ef0363ddffa
auxilia <auxilia606@gmail.com>
2024-06-26 17:48:07 +0900

    Fix : 로그인 직후에 토큰 못 가져오는 현상 수정 (util 생성 시점에 localstorage가 비어있을 수 있음.)

--- frontend/src/shared/components/Editor/utils.ts
+++ frontend/src/shared/components/Editor/utils.ts
@@ -1,16 +1,19 @@
 import { Jodit } from "jodit";
 
 import { ResDTO } from "@shared/api/post/upload";
-import { getLocalStorageItem } from "@shared/utils";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
-export const uploader = {
+type UploaderParams = {
+  token: string;
+};
+
+export const uploader = (params: UploaderParams) => ({
   url: `${baseURL}api/post/upload`,
   headers: {
-    Authorization: getLocalStorageItem("userInfo")?.token || "",
+    Authorization: params.token,
   },
   defaultHandlerSuccess: function (data: ResDTO["data"]) {
     (this as unknown as Jodit).selection.insertImage(baseURL + data.baseurl);
   },
-};
+});
